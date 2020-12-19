412174a47057415ae9fb870e818c651d42f60dcd
Logan Merchant <merchantclogan@gmail.com>
2020-12-18 18:13:36 -0700

    add Footer and Nav components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitterSquare,
+  faLinkedin,
+  faGithubSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+function Footer() {
+  return (
+    <div>
+      <a href="https://github.com/LoganMerchant">
+        <FontAwesomeIcon icon={faGithubSquare} />
+      </a>
+      <a href="https://www.linkedin.com/in/loganmerchant/">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href="https://twitter.com/bizzybark">
+        <FontAwesomeIcon icon={faTwitterSquare} />
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <h2>
+        <a href="/">Logan Merchant</a>
+      </h2>
+      <nav>
+        <ul>
+          <li>
+            <span>About</span>
+          </li>
+          <li>
+            <span>Portfolio</span>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          <li>
+            <span>Resume</span>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
