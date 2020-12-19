c1bd8b202f0294dbada9c88ddc34c1ca6b0faf65
Logan Merchant <merchantclogan@gmail.com>
2020-12-19 16:53:32 -0700

    add all project info

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
@@ -6,44 +6,99 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Project() {
+  const [projects] = useState([
+    {
+      name: "All Leaf, No Beef",
+      description:
+        "A personal gardening tracking website, built with Express.js, a MySQL database, and More!",
+      linkToProject: "https://tranquil-brushlands-81839.herokuapp.com/",
+      linkToGitHub: "https://github.com/LoganMerchant/all-leaf-no-beef",
+      linkToImage: require("../../assets/images/project-images/all-leaf-no-beef.gif"),
+    },
+    {
+      name: "Scratching Your Travel Itch",
+      description:
+        "A travel-at-home app that lets users explore more about a country.",
+      linkToProject:
+        "https://loganmerchant.github.io/scratching-your-travel-itch/",
+      linkToGitHub:
+        "https://github.com/LoganMerchant/scratching-your-travel-itch",
+      linkToImage: require("../../assets/images/project-images/scratching-your-travel-itch.gif"),
+    },
+    {
+      name: "FeedMe-a-ReadMe",
+      description:
+        "A command line application to quickly create high-quality READMEs.",
+      linkToProject: "https://github.com/LoganMerchant/feedme-a-readme",
+      linkToGitHub: "https://github.com/LoganMerchant/feedme-a-readme",
+      linkToImage: require("../../assets/images/project-images/feedme-a-readme.gif"),
+    },
+
+    {
+      name: "Budget to Go",
+      description: "A PWA for tracking a user's financial status.",
+      linkToProject: "https://infinite-cove-94181.herokuapp.com/",
+      linkToGitHub: "https://github.com/LoganMerchant/budget-to-go",
+      linkToImage: require("../../assets/images/project-images/budget-to-go.gif"),
+    },
+    {
+      name: "Password Generator",
+      description:
+        "Generates a password based on the parameters given by the user.",
+      linkToProject: "https://loganmerchant.github.io/password-generator/",
+      linkToGitHub: "https://github.com/LoganMerchant/password-generator",
+      linkToImage: require("../../assets/images/project-images/password-generator.gif"),
+    },
+    {
+      name: "Rain Check",
+      description:
+        "Displays the current weather and 5-day forecast for a searched city.",
+      linkToProject: "https://loganmerchant.github.io/rain-check/",
+      linkToGitHub: "https://github.com/LoganMerchant/rain-check",
+      linkToImage: require("../../assets/images/project-images/raincheck.gif"),
+    },
+  ]);
+
   return (
     <Row>
-      <Col sm={4}>
-        <Card className="project-card">
-          <Card.Img
-            src={require("../../assets/images/project-images/demo.gif").default}
-            alt="here we are"
-            className="card-img"
-          />
-          <div className="card-overlay">
-            <Card.ImgOverlay>
-              <Card.Title>
-                <h1>
-                  <a
-                    href="https://tranquil-brushlands-81839.herokuapp.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    All Leaf, No Beef
-                  </a>
-                </h1>
-              </Card.Title>
-              <Card.Subtitle>
-                <h3>
-                  <a
-                    href="https://github.com/LoganMerchant/all-leaf-no-beef"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <FontAwesomeIcon icon={faGithub} />
-                  </a>
-                </h3>
-              </Card.Subtitle>
-              <Card.Text>Built with Express.js, MySQL, and More!</Card.Text>
-            </Card.ImgOverlay>
-          </div>
-        </Card>
-      </Col>
+      {projects.map((project, i) => (
+        <Col sm={6}>
+          <Card className="project-card">
+            <Card.Img
+              src={project.linkToImage.default}
+              alt="here we are"
+              className="card-img"
+            />
+            <div className="card-overlay">
+              <Card.ImgOverlay>
+                <Card.Title>
+                  <h1>
+                    <a
+                      href={project.linkToProject}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {project.name}
+                    </a>
+                  </h1>
+                </Card.Title>
+                <Card.Subtitle>
+                  <h3>
+                    <a
+                      href={project.linkToGitHub}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                  </h3>
+                </Card.Subtitle>
+                <Card.Text>{project.description}</Card.Text>
+              </Card.ImgOverlay>
+            </div>
+          </Card>
+        </Col>
+      ))}
     </Row>
   );
 }
