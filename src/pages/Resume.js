c0805542984f2918fa19898fbb398310e5a630eb
Logan Merchant <merchantclogan@gmail.com>
2021-02-17 13:44:37 -0700

    update styling of resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,38 +1,49 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function Resume() {
   return (
     <Row>
       <Col sm={12} md={6}>
-        <h1>Resume</h1>
-        <p>
-          Download a PDF version{" "}
-          <a
-            href={require("../assets/downloadable-files/resume.pdf").default}
-            download="LoganMerchant-Resume"
-          >
-            <span>here</span>
-          </a>
-          !
-        </p>
+        <h1 style={{ paddingBottom: "2rem" }}>Resume</h1>
+        <h2>Front-End Proficiencies</h2>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript ES6+</li>
+          <li>React</li>
+          <li>Handlebars</li>
+          <li>Bootstrap</li>
+          <li>Metro_4</li>
+        </ul>
+
+        <h2>Back-End Proficiencies</h2>
+        <ul>
+          <li>SQL</li>
+          <li>NoSQL</li>
+          <li>Express.js</li>
+          <li>Sequelize</li>
+          <li>Mongoose</li>
+          <li>Node.js</li>
+        </ul>
       </Col>
 
       <Col sm={12} md={6}>
-        <h2>Front-End Proficiencies</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-        </ul>
-        <h2>Back-End Proficiencies</h2>
-        <ul>
-          <li>SQL Databases</li>
-          <li>NoSQL Databases</li>
-          <li>Express.js</li>
-          <li>Node.js</li>
-        </ul>
+        <h3>Click the image to download a PDF!</h3>
+        <a
+          href={require("../assets/downloadable-files/resume.pdf").default}
+          download="Logan_Merchant_Resume"
+        >
+          <Image
+            fluid
+            src={
+              require("../assets/images/misc/resume-screenshot-1.png").default
+            }
+            id="resume-thumbnail"
+          />
+        </a>
       </Col>
     </Row>
   );
